f49cb571c1c6644e9ac339ac264645c542ee93ad
Paul Mederos <paul@primeenterprise.net>
2012-07-21 13:56:14 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IOSIntent::Application.config.secret_token = '948cfb9a3d87491beab0b68645152d19ad9065da62a4874eef898ab791974b4c7583e82c7e252c6da8eb7352db8ed24c04f6142129b363c9d0da182f86fc02da'
