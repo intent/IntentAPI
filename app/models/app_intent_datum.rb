61e4579ee585bd40331a1084181c847b056758bf
Paul Mederos <paul@primeenterprise.net>
2012-07-22 11:00:34 -0400

    Basic JSON for AppIntentDatum.

--- app/models/app_intent_datum.rb
+++ app/models/app_intent_datum.rb
@@ -2,5 +2,14 @@ class AppIntentDatum < ActiveRecord::Base
 	belongs_to :app
 	belongs_to :intent
 
-		scope :intent_name, lambda { |intent| where(["intent LIKE ?", "%#{ intent.downcase.gsub(/\s+/, '') }%"]) unless intent.blank? }
+	scope :intent_name, lambda { |intent| where(["intent LIKE ?", "%#{ intent.downcase.gsub(/\s+/, '') }%"]) unless intent.blank? }
+
+	def as_json(options={})
+	  super(:only => [:name],
+	        :include => {
+	          :app => {:only => [:name, :description, :url_prefix]},
+	          :intent => {:only => [:name]}
+	        }
+	  )
+	end
 end
